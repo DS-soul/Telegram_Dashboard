eee2d7032c8b8d20ce5cd989b29a6f8356b5bdd3
Mahdi Moushekhian <mahdi.moushekhian@gmail.com>
2024-12-05 15:11:37 +0330

    Update gitignore & run.py

--- src/run.py
+++ src/run.py
@@ -1,11 +1,18 @@
+# import pandas as pd
 import streamlit as st
-
-
+from io import StringIO
 
 st.title(':zap: Telegram Dashboard')
-st.text('Hello World!')
-st.text('Mahdi')
-st.code('x = 2')
+
+uploaded_file = st.file_uploader('Choose a file')
+# if uploaded_file is not None:
+    
+#     stringio = StringIO(uploaded_file.getvalue().decode('utf-8'))
+#     string_data = stringio.read()
+#     st.write(string_data)
+    
+    # dataframe = pd.read_csv(uploaded_file)
+    # st.write(dataframe)
 
 
 
